3ff3f36f741b9b38a52b207ab6c85e140368a4e8
ALI ALRAWI <ali.developer90@gmail.com>
2017-01-03 09:09:19 +0000

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,14 @@
+class SecretsController < ApplicationController
+  before_action :logged_in?
+
+
+  def show
+  end
+
+  private
+
+  def logged_in?
+    redirect_to('/sessions/new') if ! current_user
+  end
+
+end
